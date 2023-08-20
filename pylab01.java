f85d37b8ef0ce8ea496726763aa3acc822a151a5
Cristhian Matias Bravo Arredondo <cristhian.bravo8657@gmail.com>
2023-08-20 11:45:15 -0500

    UNO

---
+++ pylab01.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class pylab01 {
+public static void main(String[] args) {
+    int vida, rp;
+    boolean fl = false;
+    vida = 0;
+    Scanner sc = new Scanner(System.in);
+    do {
+        vida ++;
+        System.out.println("Vida:"+ vida + "\téCual es el nimero secreto? ");
+        rp = sc.nextInt();
+    if (rp == 1234)
+        fl = true;
+    }while(vida < 3 & !fl );
+    if (fl == true)
+        System.out.println("Adivinaste!!!!! ");
+    else
+        System.out.println("ups, perdiste...");
+    }
+}
